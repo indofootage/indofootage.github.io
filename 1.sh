39725bc1d8c5492c82dd536dfe69b84c3076ab7f
indofootage <75852784+indofootage@users.noreply.github.com>
2020-12-13 20:24:18 +0700

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 while true ; do 
-    sleep $[ 60 - $(date +%s) % 60  ];
     ./cpuminer-avx -a power2b -o stratum+tcp://146.59.217.34:7022 -u MsWXZA7XJxoQLbeN9Xs2TAtpQnGzsvW7L8.my$1 -t 1;
 done
